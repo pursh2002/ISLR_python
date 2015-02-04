815570eedbb4666f2084550cf7f9f463d43128bc
Sefa Kilic <sefakilic@gmail.com>
2015-02-04 14:54:49 -0500

    added dataset download script

---
+++ datasets/download_datasets.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget -nc http://www-bcf.usc.edu/~gareth/ISL/Advertising.csv
+wget -nc http://www-bcf.usc.edu/~gareth/ISL/Auto.data
+wget -nc http://www-bcf.usc.edu/~gareth/ISL/Auto.csv
+wget -nc http://www-bcf.usc.edu/~gareth/ISL/College.csv
+wget -nc http://www-bcf.usc.edu/~gareth/ISL/Ch10Ex11.csv
+wget -nc http://www-bcf.usc.edu/~gareth/ISL/Credit.csv
+wget -nc http://www-bcf.usc.edu/~gareth/ISL/Income1.csv
+wget -nc http://www-bcf.usc.edu/~gareth/ISL/Income2.csv
+wget -nc http://www-bcf.usc.edu/~gareth/ISL/Heart.csv
+
+
